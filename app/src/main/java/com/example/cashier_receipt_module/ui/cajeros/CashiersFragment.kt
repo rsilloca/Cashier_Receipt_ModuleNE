54a251e43c8731f3b9a809946ec14952dfff88da
Nelson-Fuentes <40514220+Nelson-Fuentes@users.noreply.github.com>
2020-12-10 00:23:46 -0500

    layouts

--- app/src/main/java/com/example/cashier_receipt_module/ui/cajeros/CashiersFragment.kt
+++ app/src/main/java/com/example/cashier_receipt_module/ui/cajeros/CashiersFragment.kt
@@ -1,15 +1,12 @@
 package com.example.cashier_receipt_module.ui.cajeros
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cashier_receipt_module.MainActivity
@@ -36,7 +33,7 @@ class CashiersFragment : Fragment() {
         cashiersViewModel.getAllCashiers().observe(viewLifecycleOwner, Observer { cashiers ->
             cashiers?.let { cashierAdapter.setCashiers(it) }
         })
-        val cashierAddBtn = root.findViewById(R.id.add_cashier_btn) as MaterialButton
+        val cashierAddBtn = root.findViewById(R.id.add_clients_btn) as MaterialButton
         cashierAddBtn.setOnClickListener {
             (activity as MainActivity).showDialogCashier()
         }
@@ -44,7 +41,7 @@ class CashiersFragment : Fragment() {
     }
 
     private fun initRecyclerCashiers(fragment: View) {
-        cashierRecycler = fragment.findViewById(R.id.cashier_recycler) as RecyclerView
+        cashierRecycler = fragment.findViewById(R.id.clients_recycler) as RecyclerView
         cashierRecycler.adapter = cashierAdapter
         cashierRecycler.layoutManager = LinearLayoutManager(context)
     }
